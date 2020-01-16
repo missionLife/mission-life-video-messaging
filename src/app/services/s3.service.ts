22337bad483b59e5c78d6026a7747ff581d1076b
missionLife <SSJ6Porfy@hotmail.com>
2020-01-15 22:21:30 -0500

    adding timestamp to filename

--- src/app/services/s3.service.ts
+++ src/app/services/s3.service.ts
@@ -31,17 +31,18 @@ export class S3Service {
 
     const partner = sanitize(metadata.partner.split(' ').join(''));
     const fileName = sanitize(file.name).replace(/\s/g, '_');
+    const timestamp = metadata.upload.toDateString().replace(/\s/g, '_');
 
     const params = {
       Body: file,
       Bucket: 'mission-life-youtube-data-api-upload',
-      Key: `${partner}/${fileName}`,
+      Key: `${partner}/${fileName}_${timestamp}`,
       Metadata: {
         'person-metadata': JSON.stringify(metadata)
       },
       ContentType: file.type
     };
-
+    console.log('the PARAMS: ', params);
     this.initS3().putObject(params).send((err, data) => {
       if (err) {
         console.log(err, err.stack);
