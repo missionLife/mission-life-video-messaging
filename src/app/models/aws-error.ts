acebd9fb457d71c18923d17abef1745f9e9e84e7
missionLife <SSJ6Porfy@hotmail.com>
2020-04-29 19:18:02 -0400

    better error message

--- src/app/models/aws-error.ts
+++ src/app/models/aws-error.ts
@@ -1,6 +1,6 @@
 const INVALID_CREDENTIALS_ERROR_MESSAGES = {
   'User does not exist.': 'Invalid Username or Password',
-  'Incorrect username or password.': 'Incorrect Old Password',
+  'Incorrect username or password.': 'Incorrect Username or Password',
   'Password does not conform to policy: Password must have uppercase characters': 'Password must have uppercase characters',
   'Password does not conform to policy: Password must have lowercase characters': 'Password must have lowercase characters',
   'Password does not conform to policy: Password must have numeric characters': 'Password must have numeric characters',
